6ab0448dd25359604309178e0df2b9990c1dee98
SUBODH-DWIVEDI <155661739+SUBODH-DWIVEDI@users.noreply.github.com>
2024-02-18 14:29:29 +0530

    Add files via upload
    This is an project based on DOM.

---
+++ code.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    switch (e.target.id) {
+      case("grey"):
+        body.style.backgroundColor = e.target.id;
+        break;
+      case("white"):
+        body.style.backgroundColor = e.target.id;
+        break;
+      case("blue"):
+        body.style.backgroundColor = e.target.id;
+        break;
+      case("yellow"):
+        body.style.backgroundColor = e.target.id;
+        break;
+      default:
+        "none of these values";
+  
+    }
+  });
+});
